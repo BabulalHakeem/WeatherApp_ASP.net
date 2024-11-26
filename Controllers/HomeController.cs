4112439ddf1d922b41f0f9b1d5ee27760f55fdea
BabulalHakeem <babulalhakeem@gmail.com>
2024-11-26 12:41:17 +0530

    Initial commit for weather app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+using WeatherApp.Services;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly WeatherService _weatherService;
+
+        public HomeController(WeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string city)
+        {
+            if (string.IsNullOrEmpty(city))
+            {
+                ViewBag.Error = "City name cannot be empty.";
+                return View();
+            }
+
+            try
+            {
+                var weather = await _weatherService.GetWeatherAsync(city);
+                return View(weather);
+            }
+            catch
+            {
+                ViewBag.Error = "Could not fetch weather data. Please try again.";
+                return View();
+            }
+        }
+    }
+}
---
+++ Services/WeatherService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class WeatherService
+    {
+        private readonly string _apiKey;
+        private readonly HttpClient _httpClient;
+
+        public WeatherService(string apiKey)
+        {
+            _apiKey = apiKey;
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<WeatherModel> GetWeatherAsync(string city)
+        {
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={_apiKey}";
+            var response = await _httpClient.GetStringAsync(url);
+            dynamic weatherData = JsonConvert.DeserializeObject(response);
+
+            return new WeatherModel
+            {
+                City = weatherData.name,
+                Temperature = weatherData.main.temp,
+                Condition = weatherData.weather[0].description
+            };
+        }
+    }
+}
